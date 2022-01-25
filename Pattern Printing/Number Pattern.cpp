63aa1f5c13ed560f3951fbf0599b0623ff65ccd6
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2022-01-25 21:01:39 +0530

    Create Number Pattern.cpp
    Input:
    N = 3
    Output:
    1 121 12321

---
+++ Pattern Printing/Number Pattern.cpp
@@ -0,0 +1,20 @@
+vector<string> numberPattern(int N)
+    {  vector<string> v;
+        string temp;
+        for(int i=1;i<=N;i++){
+             temp="";
+       for(int j=1;j<=i;j++){
+  
+      temp+=to_string(j);
+       }
+      for(int k=i-1;k>=1;k--){
+          
+         temp+=to_string(k);
+      }
+     
+        v.push_back(temp);
+  
+      }
+ return  v;
+    }
+};
