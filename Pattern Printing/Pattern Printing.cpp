df4eb1f2057fab3bed32f2ba1d278abefe959a2a
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2022-01-25 23:02:48 +0530

    Create Pattern Printing.cpp
    Input:
    N = 3
    Output:
    * ** ***

---
+++ Pattern Printing/Pattern Printing.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    void printPattern(int N)
+    {
+       for(int i=1;i<=N;i++){
+           for(int j=1;j<=i;j++){
+               cout<<"*";
+               
+       }
+cout<<" ";
+    }
+    }
+};
