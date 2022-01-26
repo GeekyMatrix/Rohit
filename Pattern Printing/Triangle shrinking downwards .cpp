8b19255654b8cb8659320a46d50e58eaae2b140b
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2022-01-26 19:16:42 +0530

    Create Triangle shrinking downwards .cpp
    Input:
    S = Geeks
    Output:
    Geeks
    .eeks
    ..eks
    ...ks
    ....s

---
+++ Pattern Printing/Triangle shrinking downwards .cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    string triDownwards(string S){
+     int N=S.length();
+        int k=N;
+        string temp,ans="";
+        for(int i=1;i<=N;i++){
+            temp="";
+            for(int l=1;l<=i-1;l++){
+              // S.push_back(".");
+               temp+=".";
+            }
+            
+              for(int j=i-1;j<N;j++){
+	            temp+=S[j];
+              }
+           
+           ans+=temp;
+   }
+    return ans;
+    }
+};
+    
