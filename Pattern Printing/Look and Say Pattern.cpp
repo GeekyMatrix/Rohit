779a88385b8c668d1f49ea9f8e43a854f0e082e6
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2022-01-30 18:41:20 +0530

    Create Look and Say Pattern.cpp
    1
    11
    21
    1211
    111221
    Input:
    n = 5
    Output: 111221
    Explanation: The 5th row of the given pattern
    will be 111221.

---
+++ Pattern Printing/Look and Say Pattern.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+  public:
+    string lookandsay(int n) {
+        if(n==1)
+        return "1";
+        if(n==2)
+        return "11";
+        
+        string result=lookandsay(n-1);
+        string newresult="";
+        int count=1;
+        for(int i=1;i<result.size();i++)
+        {
+            if(result[i]!=result[i-1]){
+                newresult.push_back('0'+count); //converting int to char
+                newresult.push_back(result[i-1]);
+                count=1;
+            }
+            else
+            count++;
+            if(i==result.size()-1){
+                newresult.push_back('0'+count);
+                newresult.push_back(result[i]);
+            }
+        }
+        return newresult;
+    }   
+};
