355097d69a2b6d22e60729385883fae00a049045
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2022-01-31 21:05:06 +0530

    Create C++ Arrays (Sum of array) | Set 1 .cpp
    Input:
    4
    1 2 3 4
    Output:
    10

---
+++ ARRAY/C++ Arrays (Sum of array) | Set 1 .cpp
@@ -0,0 +1,10 @@
+class Solution{
+    public:
+  int getSum(int a[], int n) {
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+a[i];
+        }
+        return sum;
+    }   
+};
