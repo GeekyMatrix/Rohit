807dd8cac170e777484c2d89d177ba0c600dffc5
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2022-01-31 20:02:02 +0530

    Update Print Pattern Microsoft.cpp
    Input: N = 16
    Output: 16 11 6 1 -4 1 6 11 16
    Explaination: The value decreses until it 
    is greater than 0. After that it increases 
    and stops when it becomes 16 again.

--- Pattern Printing/Print Pattern Microsoft.cpp
+++ Pattern Printing/Print Pattern Microsoft.cpp
@@ -14,3 +14,35 @@ vector<int>v;
        return v;
     }
 };
+
+
+//2nd Approach
+class Solution{
+public:
+    vector<int> pattern(int N){
+   vector<int>v;
+   int a,b;
+     a=N;
+   v.push_back(a);
+   b=a-5;
+   a=b;
+   v.push_back(a);
+   bool temp=true;
+   
+   while(a!=N){
+       
+       if(temp && a>0){
+       b=a-5;
+       a=b;
+       v.push_back(a);
+       }
+       else{
+       b=a+5;
+       a=b;
+       v.push_back(a);
+       temp=false;
+       }
+   }
+   return v;
+    }
+};
