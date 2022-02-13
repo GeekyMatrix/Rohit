d93f8cfc6f2db927a2c3872c0315f2970b3b8061
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2022-02-13 23:15:15 +0530

    Create Index of first 1 in a sorted array of 0s and 1s .cpp
    Input : 
    arr[] = {0, 0, 0, 0, 0, 0, 1, 1, 1, 1}
    Output : 
    6
    Explanation:
    The index of first 1 in the array is 6.

---
+++ ARRAY/Index of first 1 in a sorted array of 0s and 1s .cpp
@@ -0,0 +1,13 @@
+class solution{
+public:
+    int firstIndex(int a[], int n) 
+    {
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==1)
+            return i;
+        }
+    return -1;
+    }
+    
+};
