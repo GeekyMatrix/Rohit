0982259afac952cbe1a3f6bcc57d8ea3c2138e7c
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2022-02-15 00:52:09 +0530

    Create Implement stack using array.cpp
    Input: 
    push(2)
    push(3)
    pop()
    push(4) 
    pop()
    Output: 3, 4
    Explanation: 
    push(2)    the stack will be {2}
    push(3)    the stack will be {2 3}
    pop()      poped element will be 3,
               the stack will be {2}
    push(4)    the stack will be {2 4}
    pop()      poped element will be 4

---
+++ ARRAY/Implement stack using array.cpp
@@ -0,0 +1,18 @@
+//Function to push an integer into the stack.
+void MyStack :: push(int x)
+{
+    // Your Code
+     top++;
+   arr[top] = x;
+}
+
+//Function to remove an item from top of the stack.
+int MyStack :: pop()
+{
+    // Your Code 
+     if(top == -1)
+   {
+       return -1;
+   }
+     return arr[top--];  
+}
